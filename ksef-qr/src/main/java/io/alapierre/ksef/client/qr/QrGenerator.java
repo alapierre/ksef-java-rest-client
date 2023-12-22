df88e207b87704f5527cdc631a76b8a054de9fda
Adrian Lapierre <al@alapierre.io>
2023-12-22 10:48:17 +0100

    #99 Refactor QR code generation and add custom exception handling
    QR code generation within the QrGenerator class has been refactored to include a new output stream parameter for flexibility in saving generated barcodes. Custom exception 'BarcodeGenerationException' handling has also been introduced to categorize exceptions that occur during the barcode creation process. Furthermore, the previous exceptions have been replaced with this new custom one in the method signatures. In makes code more flexible if in the future, we need to change an underline library used for barcode generation.

--- ksef-qr/src/main/java/io/alapierre/ksef/client/qr/QrGenerator.java
+++ ksef-qr/src/main/java/io/alapierre/ksef/client/qr/QrGenerator.java
@@ -8,13 +8,14 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import io.alapierre.ksef.client.AbstractApiClient.Environment;
 import io.alapierre.ksef.client.ApiException;
 import io.alapierre.ksef.client.api.InterfejsyInteraktywneFakturaApi;
+import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
-import javax.imageio.ImageIO;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -75,19 +76,43 @@ public class QrGenerator {
      * @param width The width of the barcode in pixels.
      * @param height The height of the barcode in pixels.
      * @return The generated barcode as a byte array.
-     * @throws WriterException If the barcode generation fails.
-     * @throws IOException If an error occurs during the image writing.
+     * @throws BarcodeGenerationException if a problem with barcode generation occurs
      */
-    public static byte[] generateBarcode(String barcodeText, int width, int height) throws WriterException, IOException {
+    public static byte[] generateBarcode(@NonNull String barcodeText, int width, int height)  {
+        try {
+            val buf = new ByteArrayOutputStream();
+            writeBarcode(barcodeText, width, height, buf);
+            return buf.toByteArray();
+        } catch (IOException ex) {
+            log.debug(ex.getLocalizedMessage(), ex);
+            throw new BarcodeGenerationException(ex);
+        }
+    }
 
-        QRCodeWriter barcodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, width, height);
+    /**
+     * Generates a QR Code barcode image with the given barcode text, width, and height, and writes it to the specified output stream as a PNG.
+     *
+     * @param barcodeText The text to be encoded in the barcode.
+     * @param width The width of the barcode image in pixels.
+     * @param height The height of the barcode image in pixels.
+     * @param out The output stream to write the barcode image to. The OutputStream should be closed by the calling code.
+     * @throws IOException if an I/O error occurs while writing the image to the output stream.
+     * @throws BarcodeGenerationException if a problem with barcode generation occurs
+     */
+    public static void writeBarcode(@NonNull String barcodeText, int width, int height, OutputStream out) throws IOException {
 
-        val img = MatrixToImageWriter.toBufferedImage(bitMatrix);
-        val buf = new ByteArrayOutputStream();
-        ImageIO.write(img, "PNG", buf);
+        if (width <= 0 || height <= 0) {
+            throw new BarcodeGenerationException("width and height must be positive", null);
+        }
 
-        return buf.toByteArray();
+        try {
+            QRCodeWriter barcodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, width, height);
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", out);
+        } catch (WriterException ex) {
+            log.debug(ex.getLocalizedMessage(), ex);
+            throw new BarcodeGenerationException("Failed to generate barcode: " + ex.getLocalizedMessage());
+        }
     }
 
 }
